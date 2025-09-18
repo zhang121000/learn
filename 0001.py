62a3e0e9c7d2c3b3777e0536b7c9ef8ee2936fc2
zhang121000 <zhang121000@foxmail.com>
2025-09-18 20:11:55 +0800

    将键盘的输入信息显示公屏上

---
+++ 0001.py
@@ -0,0 +1,9 @@
+# 接收用户输入的昵称、密码和性别
+nickname = input("请输入昵称：")
+password = input("请输入密码：")
+gender = input("请输入性别：")
+
+# 将用户输入的信息打印到屏幕上
+print("您输入的昵称是：", nickname)
+print("您输入的密码是：", password)
+print("您输入的性别是：", gender)
